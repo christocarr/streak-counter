d89694253773ffc050f415afa5b8c291a92bf40d
christocarr <christocarr@outlook.com>
2023-03-02 09:02:08 +0000

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import { formattedDate } from './utils';
-
-interface Streak {
-	currentCount: number;
-	startDate: string;
-	lastLoginDate: string;
-}
-
-const KEY = 'streak';
+import { Streak, KEY, buildStreak, updateLocalStorageStreak } from './utils';
 
 export function differenceInDays(dateLeft: Date, dateRight: Date): number {
 	const diffTime = Math.abs(dateLeft.getTime() - dateRight.getTime());
@@ -40,26 +33,22 @@ export function streakCounter(storage: Storage, date: Date): Streak {
 	const streakInLocalStorage = storage.getItem(KEY);
 	if (streakInLocalStorage) {
 		try {
-			const streak = JSON.parse(streakInLocalStorage || '');
+			const streak = JSON.parse(streakInLocalStorage) as Streak;
 			const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate);
 			const SHOULD_INCREMENT = state === 'increment';
 			const SHOULD_RESET = state === 'reset';
 			if (SHOULD_INCREMENT) {
-				const updatedStreak: Streak = {
-					...streak,
+				const updatedStreak = buildStreak(date, {
+					startDate: streak.startDate,
 					currentCount: streak.currentCount + 1,
 					lastLoginDate: formattedDate(date),
-				};
-				storage.setItem(KEY, JSON.stringify(updatedStreak));
+				});
+				updateLocalStorageStreak(storage, updatedStreak);
 				return updatedStreak;
 			}
 			if (SHOULD_RESET) {
-				const updatedStreak: Streak = {
-					currentCount: 1,
-					startDate: formattedDate(date),
-					lastLoginDate: formattedDate(date),
-				};
-				storage.setItem(KEY, JSON.stringify(updatedStreak));
+				const updatedStreak = buildStreak(date);
+				updateLocalStorageStreak(storage, updatedStreak);
 				return updatedStreak;
 			}
 			return streak;
@@ -68,13 +57,9 @@ export function streakCounter(storage: Storage, date: Date): Streak {
 		}
 	}
 
-	const streak = {
-		currentCount: 1,
-		startDate: formattedDate(date),
-		lastLoginDate: formattedDate(date),
-	};
+	const streak = buildStreak(date);
 
-	storage.setItem(KEY, JSON.stringify(streak));
+	updateLocalStorageStreak(storage, streak);
 
 	return streak;
 }
